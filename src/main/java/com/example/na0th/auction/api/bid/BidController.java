d3707ee90724e2727873d99ab7d8da65a1721027
na0th <105775683+na0th@users.noreply.github.com>
2025-01-13 12:57:42 +0900

    .
    .

--- src/main/java/com/example/na0th/auction/api/bid/BidController.java
+++ src/main/java/com/example/na0th/auction/api/bid/BidController.java
@@ -3,6 +3,7 @@ package com.example.na0th.auction.api.bid;
 import com.example.na0th.auction.domain.bid.dto.request.BidRequest;
 import com.example.na0th.auction.domain.bid.service.BidService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -14,9 +15,9 @@ public class BidController {
     private final BidService bidService;
 
     // 클라이언트가 "/app/bid"로 메시지를 보내면 실행됨
-    @MessageMapping("/bid")
-    @SendTo("/topic/auctions")
-    public BidRequest handleBid(BidRequest bidMessage) {
+    @MessageMapping("/bid/{auctionId}")
+    @SendTo("/topic/auctions/{auctionId}")
+    public BidRequest handleBid(@DestinationVariable String auctionId, BidRequest bidMessage) {
         bidService.processBid(bidMessage);
         return bidMessage; // 모든 구독자에게 입찰 메시지 전송(request와 response가 정확히 같음)
     }
--- src/main/java/com/example/na0th/auction/common/config/WebConfig.java
+++ src/main/java/com/example/na0th/auction/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.na0th.auction.common.config;
 import com.example.na0th.auction.common.resolver.SearchConditionResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,5 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(searchConditionResolver);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/bidhtml/{auctionId}")
+                .setViewName("forward:/bidActive.html");
     }
 }
